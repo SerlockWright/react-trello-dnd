c3ee0d4a94945bdaa5fe8c6d707640677a3dbe30
SerlockWright <vohoangtin92@gmail.com>
2023-08-01 18:12:31 +0700

    update code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import React from "react";
+import Avatar from "./assets/images/personal.jpg";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <React.Fragment>
+      <header>
+        <div className="header__container">
+          <div className="header__logo"></div>
+          <div className="header__right">
+            <div className="header__avatar">
+              <img src={Avatar} alt="Avatar" />
+            </div>
+          </div>
+        </div>
+      </header>
+      <main></main>
+    </React.Fragment>
+  );
 }
 
 export default App;
